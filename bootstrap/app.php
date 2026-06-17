3140346551a2b04bbdab168ab9850bf9c98a8030
roundsee <ronsinolast@gmail.com>
2026-06-17 14:25:52 +0700

    bootstrsp/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\EnsureUserHasRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,9 +13,19 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Alias middleware yang sudah ada
         $middleware->alias([
             'role' => EnsureUserHasRole::class,
         ]);
+
+        // Tambahan untuk mempercayai Cloudflare Tunnel
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR | 
+                     Request::HEADER_X_FORWARDED_HOST | 
+                     Request::HEADER_X_FORWARDED_PORT | 
+                     Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
