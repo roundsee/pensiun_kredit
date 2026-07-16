e68a4a2a63ef63d9cb53399d04ca90c9781f752c
roundsee <ronsinolast@gmail.com>
2026-07-16 21:13:19 +0700

    api update

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('mobile/auth')->group(function () {
     Route::post('/login', [AuthController::class, 'apiMobileLogin']);
 });
-
+//update
 Route::prefix('mobile/kb-simulasi')->group(function () {
     Route::get('/config', [KbSimulationController::class, 'mobileConfig']);
     Route::post('/calculate', [KbSimulationController::class, 'calculate']);
