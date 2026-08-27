0d344dec87e0c4c2d21f28c61f78552ff7a34aea
roundsee <ronsinolast@gmail.com>
2026-08-27 15:33:31 +0700

    new endpoint

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('mobile/auth')->group(function () {
     Route::post('/login', [AuthController::class, 'apiMobileLogin']);
 });
+
+Route::post('/calculateTenorMax', [KbSimulationController::class, 'calculateTenorMax']);
 //update
 Route::prefix('mobile/kb-simulasi')->group(function () {
     Route::get('/config', [KbSimulationController::class, 'mobileConfig']);
